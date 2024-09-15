8d3139a7b7984f827fa3f400eb86c6a6c782fb9f
pk-fusion-dev <pk.fusion.dev@gmail.com>
2024-09-15 16:47:36 +0630

    fix supabase RLS

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,8 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseConfig {
+  static final SupabaseClient client = SupabaseClient(
+    'https://fwvrwhceufgnlvkzxjet.supabase.co', // Replace with your Supabase URL
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3dnJ3aGNldWZnbmx2a3p4amV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzODQ4MTIsImV4cCI6MjA0MTk2MDgxMn0.sCw_C0y2AUjxaQWzJlHzDwKqMp0x8O425RZrdvIWkAY', // Replace with your Supabase anon key
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:developer' as dev;
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    postgrestOptions: const PostgrestClientOptions(schema: 'codelab'),
+  );
+
+  fetchData().then((value) => dev.log(value));
+  //runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+Future<String> fetchData() async {
+  final supabase = Supabase.instance.client;
+
+// Make a request
+  var data = await supabase.from('demo').select().eq('id', 1);
+  return('Data: $data');
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      title: 'Supabase Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
